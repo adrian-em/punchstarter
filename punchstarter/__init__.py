032aa312d5dcfc4828449a2ddcd556f2317533a8
Adrian Espinosa <aespinosamoreno@gmail.com>
2016-04-20 06:26:10 +0200

    create project form

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request, redirect,url_for
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -16,3 +17,33 @@ from punchstarter.models import  *
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/projects/create/", methods=['GET', 'POST'])
+def create():
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        # handle form
+        print request.form
+        now = datetime.datetime.now()
+        time_end = request.form.get('funding_end_date')
+        time_end = datetime.datetime.strptime(time_end, '%Y-%m-%d')
+        new_project = Project(
+            member_id = 1, #guest creator
+            name = request.form.get("project_name"),
+            short_description = request.form.get('short_description'),
+            long_description = request.form.get('long_description'),
+            goal_amount = request.form.get('funding_goal'),
+            time_start = now,
+            time_end = time_end,
+            time_created = now
+        )
+
+        db.session.add(new_project)
+        db.session.commit()
+
+        return redirect(url_for('create'))
+
+
+
